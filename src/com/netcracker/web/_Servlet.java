bd65b0c7e873af7b9b784cccae58d9949e5b4ae7
sharuk92 <zassyne@gmail.com>
2014-11-29 02:33:31 +0300

    organize the src folder..

---
+++ src/com/netcracker/web/_Servlet.java
@@ -0,0 +1,13 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class _Servlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		
+		
+		
+	}
+
+}
