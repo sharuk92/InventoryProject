5e36feb2cbe460e559ed7848ecfe6e2c2a3f927c
sharuk92 <zassyne@gmail.com>
2014-11-29 13:50:07 +0300

    implement the abstract class GeoEntity

--- src/com/project/components/GeoEntity.java
+++ src/com/project/components/GeoEntity.java
@@ -1,5 +1,41 @@
 package com.netcracker.web;
 
-public class GeoEntity {
+public abstract class GeoEntity {
+	
+	protected String name;
+	protected double area;
+	protected double population;
+	
+	public GeoEntity() {
+		name = " ";
+		area = 0;
+		population = 0;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setArea(double area) {
+		this.area = area;
+	}
+	
+	public void setpopulation(double population) {
+		this.population = population;
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	
+	public double getArea() {
+		return area;
+	}
+	
+	public double getpopulation() {
+		return population;
+	}
+
 
 }
