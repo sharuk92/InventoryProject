58671bcd72df0ffb7c24849006e43818b1865e9b
sharuk92 <zassyne@gmail.com>
2014-11-29 13:39:28 +0300

    using List intead..

--- src/com/project/components/Country.java
+++ src/com/project/components/Country.java
@@ -5,23 +5,33 @@ package com.project.components;
 public class Country extends GeoEntity {
 
 
-	private City[] cities;
+	private List<City> cities;
 	
 	public Country() {
 		super();
+		setCities(new ArrayList<City>());
 	}
 	
 	public Country(String name) {
 		this.name = name;
 		area = 0;
 		population = 0;
+		setCities(new ArrayList<City>());
 	}
 	
 	public Country(String name, double area, double population) {
 		this.name = name;
 		this.area = area;
 		this.population = population;
+		setCities(new ArrayList<City>());
+	}
+
+	public List<City> getCities() {
+		return cities;
+	}
+
+	public void setCities(List<City> cities) {
+		this.cities = cities;
 	}
 	
-
 }
