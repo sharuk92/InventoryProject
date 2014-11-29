098830fd96c3397b9d5340833ce08b4b487aebf9
sharuk92 <zassyne@gmail.com>
2014-11-29 13:27:17 +0300

    few changes..

--- src/com/project/components/Country.java
+++ src/com/project/components/Country.java
@@ -4,52 +4,24 @@ package com.project.components;
 
 public class Country {
 
-	private String nameCountry;
-	private double areaCountry;
-	private double populationCountry;
+
+	private City[] cities;
 	
 	public Country() {
-		nameCountry = " ";
-		areaCountry = 0;
-		populationCountry = 0;
+		super();
 	}
 	
 	public Country(String name) {
-		nameCountry = name;
-		areaCountry = 0;
-		populationCountry = 0;
+		this.name = name;
+		area = 0;
+		population = 0;
 	}
 	
 	public Country(String name, double area, double population) {
-		nameCountry = name;
-		areaCountry = area;
-		populationCountry = population;
+		this.name = name;
+		this.area = area;
+		this.population = population;
 	}
 	
-	
-	public void setnameCountry(String name) {
-		nameCountry = name;
-	}
-	
-	public void setareaCountry(double area) {
-		areaCountry = area;
-	}
-	
-	public void setpopulationCountry(double population) {
-		populationCountry = population;
-	}
-	
-	
-	public String getnameCountry() {
-		return nameCountry;
-	}
-	
-	public double getareaCountry() {
-		return areaCountry;
-	}
-	
-	public double getpopulationCountry() {
-		return populationCountry;
-	}
 
 }
---
+++ src/com/project/components/GeoEntity.java
@@ -0,0 +1,5 @@
+package com.netcracker.web;
+
+public class GeoEntity {
+
+}
